983f2a9b31942f4e70c3adbf2a51229c982e67c3
aditi-prakash <aditiprakash0338@gmail.com>
2024-07-21 11:16:50 +0530

    modify mealdeal page

--- KindMeal/src/components/HomeContainer.jsx
+++ KindMeal/src/components/HomeContainer.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-import img4 from "../assets/img4.jpg";
-import img5 from "../assets/img5.jpg";
+
 
 function HomeContainer() {
   let listItem = [
--- KindMeal/src/main.jsx
+++ KindMeal/src/main.jsx
@@ -6,11 +6,18 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ChakraProvider>
+
+
 );
