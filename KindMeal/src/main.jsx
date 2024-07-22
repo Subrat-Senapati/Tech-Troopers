54991cd8def708c4797d20480394bac2392fa31d
subrat-senapati <subratsenapati392@gmail.com>
2024-07-22 09:48:40 +0530

    modify slider

--- KindMeal/src/main.jsx
+++ KindMeal/src/main.jsx
@@ -12,11 +12,9 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
-    <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </React.StrictMode>
   </ChakraProvider>
 
 
